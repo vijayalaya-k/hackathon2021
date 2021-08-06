4825d06fdb2f222aacadc87b7cf2ef9ec540a900
vijayalaya-k <vijay13@gmail.com>
2021-08-06 10:28:57 -0700

    Add files via upload

---
+++ EOBforReal/LaunchPoint.swift
@@ -0,0 +1,18 @@
+//
+//  LaunchPoint.swift
+//  EOBforReal
+//
+//  Created by Paul Hudson on 28/04/2019.
+//  Copyright © 2019 Hacking with Swift. All rights reserved.
+//
+
+import Foundation
+
+struct LaunchPoint: Decodable {
+    let name: String
+    let dates: String
+    let field: String
+    let bio: String
+    let country: String
+    let source: String
+}
